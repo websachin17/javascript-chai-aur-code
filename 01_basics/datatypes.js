c8f2cc0e3a7db96e190ddd5568d394a562942230
websachin17 <135368424+websachin17@users.noreply.github.com>
2023-12-10 06:56:23 +0000

    data types and variables

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,4 @@
+ alert(3 + 3) // this will give error becoz it is not a browser
+
+console.log(" sachin " , 3 + 3)
+  
---
+++ 01_basics/variable.js
@@ -0,0 +1,13 @@
+let account_email = " sachin@gmail.com "
+
+const password = "12345"
+
+city = "jaipur"
+
+account_email = "ganga@gmail.com"
+
+
+ city =" punjab "
+
+
+console.table([ account_email, password, city])
